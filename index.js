5d59fa8f58a0e2a11310529f26d9a75c860c60fe
Kaan <kaanasutay10@gmail.com>
2024-02-20 23:35:49 +0300

    call personnelRouter

--- index.js
+++ index.js
@@ -53,6 +53,7 @@ app.all('/', (req, res) => {
 
 // /departments
 app.use('/departments', require('./src/routes/department.router'))
+app.use('/personnels', require('./src/routes/personnel.router'))
 
 /* ------------------------------------------------------- */
 
