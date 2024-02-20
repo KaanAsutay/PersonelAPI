fd0b0998856478df0349fa46acf746ff3f0955a9
Kaan <kaanasutay10@gmail.com>
2024-02-20 23:34:50 +0300

    personelRouter

--- src/routes/personnel.router.js
+++ src/routes/personnel.router.js
@@ -5,7 +5,19 @@
 const router = require('express').Router()
 /* ------------------------------------------------------- */
 
+const personnel = require('../controllers/personnel.controller')
 
+// URL: /deparments
+
+router.route('/')
+    .get(personnel.list)
+    .post(personnel.create)
+
+router.route('/:id')
+    .get(personnel.read)
+    .put(personnel.update)
+    .patch(personnel.update)
+    .delete(personnel.delete)
 
 /* ------------------------------------------------------- */
 module.exports = router
