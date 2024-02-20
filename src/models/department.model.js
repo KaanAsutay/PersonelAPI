25711a5c4af0c0db17419d9d1e5ee2a483fb06b8
Kaan <kaanasutay10@gmail.com>
2024-02-20 22:14:53 +0300

    departmentModel

--- src/models/department.model.js
+++ src/models/department.model.js
@@ -4,3 +4,17 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+
+const DepartmentSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    }
+
+}, {collection: "departments", timestamps: true})
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('Department', DepartmentSchema)
