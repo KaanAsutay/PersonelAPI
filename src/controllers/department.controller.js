f978eea9f871eb3628a3e52c545cc0ca724da496
Kaan <kaanasutay10@gmail.com>
2024-02-20 22:21:32 +0300

    departmentController list

--- src/controllers/department.controller.js
+++ src/controllers/department.controller.js
@@ -2,3 +2,38 @@
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
+
+const Department = require('../models/department.model')
+
+module.exports = {
+
+    list: async (req, res) => {
+
+        // const data = await Department.find(search).sort(sort).skip(skip).limit(limit)
+        const data = await res.getModelList(Department)
+
+        res.status(200).send({
+            error: false,
+            data, // data: data
+            
+        })
+
+    },
+
+    create: async (req, res) => {
+
+    },
+
+    read: async (req, res) => {
+
+    },
+
+    update: async (req, res) => {
+
+    },
+
+    delete: async (req, res) => {
+
+    }
+
+}
