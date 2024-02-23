d41e11c365925158a97b8c2042e54fc287e67543
Kaan <kaanasutay10@gmail.com>
2024-02-24 01:55:28 +0300

    update

--- src/routes/personnel.router.js
+++ src/routes/personnel.router.js
@@ -6,6 +6,7 @@ const router = require('express').Router()
 /* ------------------------------------------------------- */
 
 const personnel = require('../controllers/personnel.controller')
+const permissions = require('../middlewares/permissions')
 
 // URL: /deparments
 
@@ -14,14 +15,14 @@ router.post('/login', personnel.login)
 router.all('/logout', personnel.logout)
 
 router.route('/')
-    .get(personnel.list)
-    .post(personnel.create)
+    .get(permissions.isAdmin, personnel.list)
+    .post(permissions.isAdmin, personnel.create)
 
 router.route('/:id')
-    .get(personnel.read)
-    .put(personnel.update)
-    .patch(personnel.update)
-    .delete(personnel.delete)
+    .get(permissions.isAdminOrOwner, personnel.read)
+    .put(permissions.isAdminOrOwner, personnel.update)
+    .patch(permissions.isAdminOrOwner, personnel.update)
+    .delete(permissions.isAdmin, personnel.delete)
 
 /* ------------------------------------------------------- */
 module.exports = router
