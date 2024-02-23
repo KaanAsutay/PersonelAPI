5e5593fd27832df6e41da9f8b338fd150861c481
Kaan <kaanasutay10@gmail.com>
2024-02-24 00:29:02 +0300

    call auth

--- index.js
+++ index.js
@@ -74,8 +74,11 @@ app.all('/', (req, res) => {
     })
 })
 
+// /auth
+app.use('/auth', require('./src/routes/auth.router'))
 // /departments
 app.use('/departments', require('./src/routes/department.router'))
+// / personnels
 app.use('/personnels', require('./src/routes/personnel.router'))
 
 /* ------------------------------------------------------- */
