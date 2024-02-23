984398e02d65bc34107b4ae64df8bbf3e5d49392
Kaan <kaanasutay10@gmail.com>
2024-02-24 02:02:35 +0300

    refresh

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -76,7 +76,30 @@ module.exports = {
 
     refresh: async (req, res) => {
 
-        
+        const refreshToken = req.body?.token?.refresh || null
+
+        if (refreshToken) {
+
+            const jwtData = jwt.verify(refreshToken, process.env.REFRESH_KEY)
+
+            if (jwtData) {
+
+                const checkUser = await checkUserAndSetToken(jwtData, false)
+                if (checkUser.error) {
+                    res.errorStatusCode = 401
+                    throw new Error(checkUser.message)
+                } else {
+                    res.send(checkUser)
+                }
+
+            } else {
+                res.errorStatusCode = 401
+                throw new Error('Wroong JWT Token')
+            }
+        } else {
+            res.errorStatusCode = 401
+            throw new Error('Please entry token.refresh')
+        }
     },
 
     logout: async (req, res) => {
