f28fa8dc7fa020f4130d2ed883fe087aa70e4b98
Kaan <kaanasutay10@gmail.com>
2024-02-26 00:01:26 +0300

    add swagger list and create

--- src/controllers/department.controller.js
+++ src/controllers/department.controller.js
@@ -9,6 +9,19 @@ module.exports = {
 
     list: async (req, res) => {
 
+        /*
+            #swagger.tags = ["Departments"]
+            #swagger.summary = "List Departments"
+            #swagger.description = `
+                You can send query with endpoint for search[], sort[], page and limit.
+                <ul> Examples:
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                    <li>URL/?<b>page=2&limit=1</b></li>
+                </ul>
+            `
+        */
+
         // const data = await Department.find(search).sort(sort).skip(skip).limit(limit)
         const data = await res.getModelList(Department)
 
@@ -37,6 +50,18 @@ module.exports = {
     },
 
     create: async (req, res) => {
+
+        /*
+            #swagger.tags = ["Departments"]
+            #swagger.summary = "Create Department"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    name: 'Test Department'
+                }
+            }
+        */
 
         const data = await Department.create(req.body)
 
