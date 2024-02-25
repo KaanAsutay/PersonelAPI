93913211b769415bd61d9b05725e585bf62295b5
Kaan <kaanasutay10@gmail.com>
2024-02-26 00:03:37 +0300

    add swagger list

--- src/controllers/personnel.controller.js
+++ src/controllers/personnel.controller.js
@@ -9,6 +9,19 @@ module.exports = {
 
     list: async (req, res) => {
 
+        /*
+            #swagger.tags = ["Personnels"]
+            #swagger.summary = "List Personnels"
+            #swagger.description = `
+                You can send query with endpoint for search[], sort[], page and limit.
+                <ul> Examples:
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                    <li>URL/?<b>page=2&limit=1</b></li>
+                </ul>
+            `
+        */
+
         const data = await res.getModelList(Personnel, {}, 'departmentId')
 
         res.status(200).send({
