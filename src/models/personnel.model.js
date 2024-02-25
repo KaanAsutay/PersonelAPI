2ef0d0aaa87e8f6c3e0598f62e03943bf0fc60ab
Kaan <kaanasutay10@gmail.com>
2024-02-26 00:14:03 +0300

    update code

--- src/models/personnel.model.js
+++ src/models/personnel.model.js
@@ -19,6 +19,8 @@ const PersonnelSchema = new mongoose.Schema({
         trim: true,
         required: true,
         unique: true,
+        dataType: 'string',
+        exampleUsing: "username: test"
     },
 
     password: {
